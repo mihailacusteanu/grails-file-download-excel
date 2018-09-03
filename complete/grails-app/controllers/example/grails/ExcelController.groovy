954c021fb66cb9276399017deb40dab5401d4ba6
sdelamo <sergio.delamo@softamo.com>
2018-09-03 13:11:01 +0200

    Fix compilation issue

--- complete/grails-app/controllers/example/grails/ExcelController.groovy
+++ complete/grails-app/controllers/example/grails/ExcelController.groovy
@@ -25,7 +25,7 @@ class ExcelController implements GrailsConfigurationAware { // <1>
 
     def index() {
         response.status = OK.value() // <2>
-        response.setHeader "Content-disposition", "attachment; filename=${BookExcelService.FILENAME}" // <3>
+        response.setHeader "Content-disposition", "attachment; filename=${BookExcelService.EXCEL_FILENAME}" // <3>
         response.contentType = "${xlsxMimeType};charset=${encoding}" // <4>
         OutputStream outs = response.outputStream
         bookExcelService.exportExcelFromBooks(outs, bookService.findAll()) // <5>
